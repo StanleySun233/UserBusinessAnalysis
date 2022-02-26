f313a5cb0a4a6e893cdf995349b3f9173be71abb
Stanley Sun <staleysun233@163.com>
2022-02-26 19:16:13 +0800

    update: first push

---
+++ dataset/download.py
@@ -0,0 +1,18 @@
+import os
+
+url1 = 'https://pan.baidu.com/s/1O7kT_S_GA8S_dWZasmif9w?pwd=zbm2 '
+'''
+停顿词
+链接：https://pan.baidu.com/s/1O7kT_S_GA8S_dWZasmif9w?pwd=zbm2 
+提取码：zbm2
+'''
+
+url2 = 'https://pan.baidu.com/s/1bDDXkBSHmPAcEsmXkC-uhg?pwd=osm4'
+'''
+链接：https://pan.baidu.com/s/1bDDXkBSHmPAcEsmXkC-uhg?pwd=osm4 
+提取码：osm4
+'''
+
+os.system("start " + url1)
+
+os.system("start " + url2)
